1c655bb75d427962012ab4b5b8daa03be9adcfbe
unknown <ahedqaood19@gmail.com>
2022-04-10 00:52:45 +0300

    nothig

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/', function () {
         $food_box = getBox('box_name', 'food_box')->first();
