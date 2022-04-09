d92da083d59e418f2a0960083a9135dba9034032
unknown <ahedqaood19@gmail.com>
2022-04-10 01:49:29 +0300

    fix a bug

--- resources/views/open_code.blade.php
+++ resources/views/open_code.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -36,4 +36,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-guest-layout>
